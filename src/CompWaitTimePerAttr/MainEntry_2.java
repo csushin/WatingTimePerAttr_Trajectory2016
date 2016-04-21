632a8e9d9c513645ba93cc72f1c66e3d44844c2c
asushin <csushin1004@gmail.com>
2016-04-20 18:37:49 -0700

    needs to consider the definition of waiting time.

--- src/CompWaitTimePerAttr/MainEntry_2.java
+++ src/CompWaitTimePerAttr/MainEntry_2.java
@@ -7,56 +7,76 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainEntry_2 {
 	public static void main(String[] args){
-		MainEntry self = new MainEntry();
+		MainEntry_2 self = new MainEntry_2();
 		String readerpath = "E:\\EclipseProjects\\attr_clocked_sequences_actual.csv";
-		String writerpath = "E:\\EclipseProjects\\timeresult_actual.csv";
 		BufferedReader reader = null;
-		BufferedWriter writer = null;
-		double unitTimeAvg=0, timeAvg=0, attractionAvg = 0, lineCount=0, repeatedAttrAvg = 0;
+		int attractionLength = 86;
+		int timesliceLength = 96;
+		int unitTime = 6;
+		double[][] averageTime = new double[attractionLength][timesliceLength];
+		double[][] outliersVid = new double[attractionLength][timesliceLength];
+		double[][] stdTime = new double[attractionLength][attractionLength];
+		String[][] visitors = new String[attractionLength][timesliceLength];
+		HashMap<String, String[]> visitorSeq = new HashMap<String, String[]>();
+		HashMap<String, double[][]> WTPerVisitor = new HashMap<String, double[][]>();
+		int totalTime = 0;
 		try{
 			String eachline;
 			reader = new BufferedReader(new FileReader(readerpath));
-			writer = new BufferedWriter(new FileWriter(writerpath));
-			String head = "visitorID,UnitTime,TotalTime,TotalAttractions,RepeatedTimes";
-			writer.write(head);
-			writer.newLine();
-			NumberFormat formatter = new DecimalFormat("#0.00");
 			while((eachline = reader.readLine()) != null){
-				lineCount++;
 				String visitorID = eachline.split(";")[0].replace("\"", "");
-				if(visitorID=="1983765"){
-					System.out.println(eachline);
-				}
 				String sequence = eachline.split(";")[1].replace("\"", "");
 				String[] attractions = sequence.split("-");
-				int totalTimeCount = self.queryTime(attractions);
-				timeAvg+=totalTimeCount;
-				int totalAttractionCount = self.queryAttractions(attractions);
-				attractionAvg+=totalAttractionCount;
-				double MinPerAttr = (double) totalTimeCount / (double)totalAttractionCount;
-				unitTimeAvg+=MinPerAttr;
-				int repeatedAttractionCount = self.queryRepeatedAttractions(attractions);
-				repeatedAttrAvg+=repeatedAttractionCount;
-				String toWrite = visitorID + "," + formatter.format(MinPerAttr) + "," + totalTimeCount + "," + totalAttractionCount+","+repeatedAttractionCount/(double)totalAttractionCount;
-				writer.write(toWrite);
-				writer.newLine();
+				visitorSeq.put(visitorID, attractions);
+				totalTime = attractions.length;
+				double[][] time = new double[attractionLength][timesliceLength];
+				for(int i=0; i<attractions.length; i++){
+					if(!attractions[i].equals("0")){
+						int minute = i/6;
+						int attId = Integer.valueOf(attractions[i]);
+						time[attId][minute]++;
+						averageTime[attId][minute]++;
+					}
+				}
+				WTPerVisitor.put(visitorID, time);
 			}
-			timeAvg/=lineCount;
-			attractionAvg/=lineCount;
-			unitTimeAvg/=lineCount;
-			repeatedAttrAvg/=lineCount;
-			String toWrite = "StatAvg,"+unitTimeAvg+","+timeAvg+","+attractionAvg+","+repeatedAttrAvg;
-			writer.write(toWrite);
-			writer.newLine();
-			
-			writer.flush();
-			writer.close();
 			reader.close();
 		}catch(IOException e){
 			System.out.println(e);
 		}
+		Iterator visitorIt = WTPerVisitor.entrySet().iterator();
+		while(visitorIt.hasNext()){
+			Map.Entry pair = (Map.Entry)visitorIt.next();
+		}
+//		for(int t = 0; t<totalTime; t++){
+//			Iterator visitorIt = visitorSeq.entrySet().iterator();
+//			while(visitorIt.hasNext()){
+//				Map.Entry pair = (Map.Entry)visitorIt.next();
+//				String[] seq = (String[]) pair.getValue();
+//				String visitorId = (String) pair.getKey();
+//				int minute = t/unitTime;
+//				if(!seq[t].equals("0")){
+//					int attId = Integer.valueOf(seq[t]);
+//					averageTime[attId][minute]++;
+//					if(!visitors[attId][minute].contains(visitorId)){
+//						if(visitors[attId][minute] == null){
+//							visitors[attId][minute] = visitorId;
+//						}
+//						else{
+//							visitors[attId][minute]+=(","+visitorId);
+//						}
+//					}
+//					
+//					
+//				}
+//				
+//			}
+//		}
 	}
 }
